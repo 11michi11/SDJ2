23c49ff3f545bcf607d343db2c94ff1c38ed1c74
11michi11 <11michi11@gmail.com>
2018-02-07 18:19:21 +0100

    Queue update

--- Exercise01.01/src/utility/collection/DemoQueue.java
+++ Exercise01.01/src/utility/collection/DemoQueue.java
@@ -3,26 +3,23 @@ package utility.collection;
 public class DemoQueue {
 	
 	public static void main(String[] args) {
-		BoundedArrayStringQueue queue = new BoundedArrayStringQueue(10);
+		Queue<String> queue = new Queue<String>();
 		
 		
-		queue.enqueue("Miœka");
-		queue.enqueue("Micha³");
-		queue.enqueue("Jakub");
-		queue.enqueue("Angel");
-		queue.enqueue("Samuel");
-		queue.enqueue("Jackson");
+		queue.push("Miœka");
+		queue.push("Micha³");
+		queue.push("Jakub");
+		queue.push("Angel");
+		queue.push("Samuel");
+		queue.push("Jackson");
 		
 		System.out.println(queue);
-		System.out.println(queue.dequeue());
+		System.out.println(queue.pop());
 		System.out.println(queue);
 		System.out.println(queue.first());
 		System.out.println(queue.indexOf("Micha³"));
 		System.out.println(queue.isEmpty());
 		System.out.println(queue.size());
-		
-		
-		
 	}
 
 }
--- Exercise01.01/src/utility/collection/Node.java
+++ Exercise01.01/src/utility/collection/Node.java
@@ -5,9 +5,26 @@ public class Node<T> {
 	private Node next;
 	private T value;
 
-	public Node(Node next, T value) {
-		this.next = next;
+	public Node(T value) {
 		this.value = value;
+	}
+	
+	public boolean hasNext() {
+		if(next == null)
+			return false;
+		return true;
+	}
+	
+	public Node next() {
+		return next;
+	}
+	
+	public T getValue() {
+		return value;
+	}
+
+	public void setNext(Node node) {
+		next = node;
 	}
 
 }
