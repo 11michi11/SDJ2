065f91263ff0a85607660a5d28d3c1e4e4dc79cf
11michi11 <11michi11@gmail.com>
2018-02-22 14:29:07 +0100

    Exercise 5

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,7 +2,18 @@
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		
+		Runnable r = new ThreadDemo();
+		Thread t = new Thread(r);
+		t.start();
+		
+		Runnable z = new PrintingLetters();
+		Thread t1 = new Thread(z);
+		t1.start();
+		
+		
+		for(int i=0;i<50;i++)
+			System.out.println("MAIN");
 	}
 
 }
