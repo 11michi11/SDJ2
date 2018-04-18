2cc2c942dd705bc0f80e34826c222c91f1b866f0
11michi11 <11michi11@gmail.com>
2018-04-18 08:34:54 +0200

    Update

--- Exercise 12.01/src/DateTime.java
+++ Exercise 12.01/src/DateTime.java
@@ -2,24 +2,22 @@ import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-
 public class DateTime {
-	
-	 private Date date;
 
-	 public DateTime() {
-	 date = (Date) Calendar.getInstance().getTime(); // store current time
-	 }
-	 
-	 public String getTimestamp() {
-	 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-	 return sdf.format(date);
-	 }
-	 
-	 public String getSortableDate(){
-	 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-	 return sdf.format(date);
-	 }
-	 
-	
+	private Date date;
+
+	public DateTime() {
+		date = (Date) Calendar.getInstance().getTime(); // store current time
+	}
+
+	public String getTimestamp() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		return sdf.format(date);
+	}
+
+	public String getSortableDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		return sdf.format(date);
+	}
+
 }
--- Exercise 12.01/src/Log.java
+++ Exercise 12.01/src/Log.java
@@ -1,25 +1,30 @@
 import java.io.File;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Log {
-	
+
 	private File file;
 	private static Log instance;
 	private Queue<LogLine> logline;
-	
+
 	private Log() {
+		logline = new LinkedList<LogLine>();
 	}
 	
-	private void add(LogLine logline) {
+	public Log getInstance() {
+		if(instance == null)
+			instance = new Log();
+		return instance;
+	}
+
+	public void add(LogLine logline) {
 		this.logline.add(logline);
 		System.out.println(logline);
 	}
-	
-	private Queue<LogLine> getAll() {
+
+	public Queue<LogLine> getAll() {
 		return logline;
 	}
-	
 
 }
